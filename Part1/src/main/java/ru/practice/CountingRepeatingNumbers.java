70e21d0be070736b5bf24d02908745be6636e3e9
Denis <neothat@mail.ru>
2022-03-15 21:57:24 +0300

    Repetition of the first task

--- Part1/src/main/java/ru/practice/CountingRepeatingNumbers.java
+++ Part1/src/main/java/ru/practice/CountingRepeatingNumbers.java
@@ -16,6 +16,8 @@ public class CountingRepeatingNumbers {
 
         System.out.println(countDuplicateCharactersVar1(string));
         System.out.println(countDuplicateCharactersVar2(string));
+        System.out.println(countDuplicateCharactersVar1_1(string));
+        System.out.println(countDuplicateCharactersVar2_1(string));
     }
 
     private static Map<Character, Integer> countDuplicateCharactersVar1(String str) {
@@ -43,6 +45,44 @@ public class CountingRepeatingNumbers {
 
         return str.chars()
                 .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    }
+
+    /**
+     * Методы с поддержкой ASCII и Юникода
+     **/
+
+    private static Map<String, Integer> countDuplicateCharactersVar1_1(String str) {
+        Map<String, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+
+//            int cp = str.codePointAt(i);
+//            String ch = String.valueOf(Character.toChars(cp));
+//            if (Character.charCount(cp) == 2) { // 2 означает суррогатную пару
+//                i++;
+//            }
+
+            String ch = String.valueOf(Character.toChars(str.codePointAt(i)));
+            if (i < str.length() - 1 && str.codePointCount(i, i + 2) == 1) {
+                i++;
+            }
+
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+        return result;
+    }
+
+    private static Map<String, Long> countDuplicateCharactersVar2_1(String str) {
+
+//        Map <String, Long> result = str.codePoints()
+//                .mapToObj(c -> String.valueOf(Character.toChars(c)))
+//                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+//
+//        return result;
+
+        return str.codePoints()
+                .mapToObj(c -> String.valueOf(Character.toChars(c)))
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
     }
 }
